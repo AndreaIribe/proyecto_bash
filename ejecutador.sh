f161bfb31c1d4f4934c7e850a41798242b0eabbd
Andrea Iribe <andreatorresiribe@gmail.com>
2024-04-07 10:32:19 -0700

    scrip listo

---
+++ ejecutador.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Este scrip pregunta por todos los paises
+
+#Sacar todos los paises posibles
+todos=$(cut -d"," -f1 paises.txt | tail -n+2)
+
+for mivariable in $todos
+do
+  echo "Preguntando por $mivariable"
+  bash pregunton.sh $mivariable
+done
